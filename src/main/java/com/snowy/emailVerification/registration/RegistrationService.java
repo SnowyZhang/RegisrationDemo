cd9a29b77f8bdf07b90cc355211c7b537095aff1
Snowy <zhangxsnowy@outlook.com>
2024-11-19 22:50:44 +0800

    增加registration模块代码

---
+++ src/main/java/com/snowy/emailVerification/registration/RegistrationService.java
@@ -0,0 +1,12 @@
+package com.snowy.emailVerification.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+    public static String register(RegistrationRequest request) {
+
+        return "works";
+    }
+}
