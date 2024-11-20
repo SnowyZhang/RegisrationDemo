b1cf217bdfbf52b0215954b92ff7fc365076436f
Snowy <zhangxsnowy@outlook.com>
2024-11-20 16:18:32 +0800

    增加邮件验证有效与否功能（待完善，暂时直接返回true）

--- src/main/java/com/snowy/emailVerification/registration/RegistrationService.java
+++ src/main/java/com/snowy/emailVerification/registration/RegistrationService.java
@@ -1,12 +1,34 @@
 package com.snowy.emailVerification.registration;
 
+import com.snowy.emailVerification.appUser.AppUser;
+import com.snowy.emailVerification.appUser.AppUserRepository;
+import com.snowy.emailVerification.appUser.AppUserRole;
+import com.snowy.emailVerification.appUser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
-    public static String register(RegistrationRequest request) {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+    public String register(RegistrationRequest request) {
+        boolean isEmailValid = emailValidator.test(request.getEmail());
 
-        return "works";
+        if (!isEmailValid) {
+            throw new IllegalStateException("邮箱不合法");
+        }else{
+            System.out.println("邮箱合法");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
